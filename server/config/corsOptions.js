ffc0e9a7e4fbed30cd8d4b017bf8241f28017616
Kanem <kanemdeng@email.com>
2023-06-19 10:32:22 -0700

    created user and note schema model

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -3,7 +3,7 @@ const allowedOrigins = require('./allowedOrigins')
 const corsOptions = {
   origin: (origin, callback) => {
     // !origin 
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       // first arg error:null, origin:true
       callback(null, true)
     } else {
