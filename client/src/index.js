0f1b8889fad52dacf2e2852053ad7acee6215e5f
Kanem <kanemdeng@email.com>
2023-06-22 19:05:43 -0700

    created provider for app

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,20 @@ import './index.css'
 import App from './App'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CssBaseline } from '@mui/material'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
+    <Provider store={store}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
 
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
