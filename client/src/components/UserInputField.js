a92248ec23e9c1b3b542c6a45a702976501480c7
Kanem <kanemdeng@email.com>
2023-06-30 15:05:32 -0700

    finished basic logic

--- client/src/components/UserInputField.js
+++ client/src/components/UserInputField.js
@@ -1,12 +1,19 @@
-import React, { useRef, useState } from 'react'
-import FormControl from '@mui/material/FormControl'
+import React, { useRef, useState, useEffect } from 'react'
+import { FormControl, FormLabel, Checkbox, FormControlLabel } from '@mui/material'
 import OutlinedInput from '@mui/material/OutlinedInput'
 import Box from '@mui/material/Box'
 import FormHelperText from '@mui/material/FormHelperText'
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked'
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
+import { green, blue, red } from '@mui/material/colors'
 
-export default function UserInputField({ placeholder }) {
+export default function UserInputField({ inputs, state, setState, validation }) {
+
+  const { placeholder, text, error, correct, type } = inputs
+
   const [focusedInput, setFocusedInput] = useState(null)
-
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirm, setShowConfirm] = useState(false)
 
   const handleFocus = (inputName) => {
     setFocusedInput(inputName)
@@ -17,25 +24,52 @@ export default function UserInputField({ placeholder }) {
 
   }
 
-  const isInputFocused = (inputName) => {
-    return focusedInput === inputName
+  const handleChange = (e) => {
+    setState(e.target.value)
   }
 
-
+  const handleShowPassword = (e) => {
+    setShowPassword(e.target.checked)
+  }
+  const handleShowConfirm = (e) => {
+    setShowConfirm(e.target.checked)
+  }
 
   return (
-    <Box component="form" noValidate autoComplete="off">
-      <FormControl sx={{ width: '25ch' }}>
-        <OutlinedInput
-          placeholder={placeholder}
-          onFocus={() => handleFocus(placeholder)}
-          onBlur={handleBlur}
 
-        />
-        <FormHelperText>
-          {isInputFocused(placeholder) ? '' : 'Helper text'}
-        </FormHelperText>
-      </FormControl>
-    </Box>
+    <FormControl sx={{ width: '85%' }}>
+      <FormLabel
+        error={state.length && !validation && focusedInput === null ? true : false}
+      >{placeholder}*</FormLabel>
+      <OutlinedInput
+        required
+        placeholder={placeholder}
+        onFocus={() => handleFocus(placeholder)}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        type={
+          type === 'password' && showPassword ? 'text' :
+            type === 'password' && showConfirm ? 'text' :
+              type
+        }
+        error={state.length && !validation && focusedInput === null ? true : false}
+
+        endAdornment={
+          validation ? <RadioButtonCheckedIcon position="end" sx={{ color: green[500] }} />
+            : ''
+        }
+      />
+      <FormHelperText>
+        {validation ? correct : state.length && !validation && focusedInput === null ? error : text}
+      </FormHelperText>
+
+      {placeholder === 'Password' ?
+        <FormControlLabel control={<Checkbox onChange={handleShowPassword} />} label="Show Password" /> :
+        placeholder === 'Confirm Password' ?
+          <FormControlLabel control={<Checkbox onChange={handleShowConfirm} />} label="Show Confirm Password" />
+          : ''
+      }
+    </FormControl>
+
   )
 }
