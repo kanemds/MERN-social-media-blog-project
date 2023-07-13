6ff56b9a69a12d557528c613fed6d4fdfaec7370
Kanem <kanemdeng@email.com>
2023-07-12 17:38:20 -0700

    created authApiSlice

---
+++ client/src/pages/auth/authApiSlice.js
@@ -0,0 +1,37 @@
+import { apiSlice } from "../../app/api/apiSlice"
+import { logOut } from "./authSlice"
+
+const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: builder => ({
+    login: builder.mutation({
+      query: credentials => ({
+        url: '/auth',
+        method: 'POST',
+        body: credentials
+      })
+    }),
+    sendLogOut: builder.mutation({
+      query: () => ({
+        url: '/auth/logout',
+        method: 'POST'
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled
+          dispatch(logOut())
+          dispatch(apiSlice.util.resetApiState()) // clear out the cache data
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }),
+    refresh: builder.mutation({
+      query: () => ({
+        url: '/auth/refresh',
+        method: 'GET'
+      })
+    })
+  })
+})
+
+export const { useLoginMutation, useSendLogOutMutation, useRefreshMutation } = authApiSlice
