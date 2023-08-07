c4c71921ae84788d1458a92f6fbe169400326340
Kanem <kanemdeng@email.com>
2023-08-06 23:04:33 -0700

    added sticky on search bar

--- client/src/pages/mainPage/MainContent.js
+++ client/src/pages/mainPage/MainContent.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ActiveCalender from './ActiveCalender'
-import { Box, Button, Paper, Container, Typography } from '@mui/material'
+import { Box, Button, Paper, Container, Typography, AppBar, Toolbar } from '@mui/material'
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles'
 import Grid from '@mui/material/Unstable_Grid2'
 import BodyElements from './BodyElements'
 import Note from './Note'
 import SearchBar from '../../components/SearchBar'
+import './mainPageStyle.css'
 
 const theme = createTheme({
   breakpoints: {
@@ -58,12 +59,11 @@ const MainContent = () => {
   // const currentDay = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
 
   return (
-    <Container sx={{ flexGrow: 1 }} maxWidth='true'>
-      <Grid container  >
+    <Container sx={{ flexGrow: 1, mt: '10px' }} maxWidth='true'>
 
-
+      <Grid container sx={{ position: 'sticky', top: '70px', backgroundColor: 'lightblue', zIndex: 10 }}>
         <ThemeProvider theme={theme}  >
-          <Grid xs={12} sm={12} md={7.3} lg={8.5} xl={9} xxl={10}>
+          <Grid xs={12} sm={12} md={7.3} lg={8.5} xl={9} xxl={10} >
             <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%' }}>
               <SearchBarWidth >
                 <SearchBar />
@@ -76,7 +76,6 @@ const MainContent = () => {
               </Box>
             </Box>
           </Grid>
-
           <Root xs={12} sm={12} md={4.7} lg={3.5} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
             <ActiveCalender />
           </Root >
@@ -85,9 +84,33 @@ const MainContent = () => {
       </Grid>
 
       <Box sx={{ flexGrow: 1 }}  >
+        <ThemeProvider theme={theme}  >
+          <Grid container spacing={2} columns={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 15, xxl: 12 }}>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+          </Grid>
+        </ThemeProvider>
+        <Typography>Others</Typography>
+        <ThemeProvider theme={theme}  >
+          <Grid container spacing={2} columns={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 15, xxl: 12 }}>
+
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+            <Grid xs={12} sm={6} md={4} lg={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}><Note /></Grid>
+          </Grid>
 
 
-
+        </ThemeProvider>
+        <Typography>Others</Typography>
         <ThemeProvider theme={theme}  >
           <Grid container spacing={2} columns={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 15, xxl: 12 }}>
 
