176e3d094634ef7f5d415a8352e16ed6d30f23f1
Kanem <kanemdeng@email.com>
2023-08-22 16:29:35 -0700

    able to navigate to single log and edit page

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -11,15 +11,19 @@ import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined'
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined'
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined'
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined'
+import { useNavigate } from 'react-router-dom'
 
 export default function Note({ blog, username = null }) {
-  // const title = "B.C. man's swoon-worthy proposal at van Gogh art exhibit to be cherished after his death"
-  // const paragraph = `BURNABY, BC., August 3, 2023 - Scenes from Lougheed Mall as customers shopping at Hudson's Bay over the weekend were met with closing sale signage: "This store is permanently closing.", in Burnaby, B.C. on August 3 , 2023. (NICK PROCAYLO/PNG) 00101885A [PNG Merlin Archive] PHOTO BY NICK PROCAYLO /00101885A`
+
+  const navigate = useNavigate()
 
   const [title, setTitle] = useState(blog?.title)
   const [text, setText] = useState(blog?.text)
   const [images, setImage] = useState(blog?.images[0] || noteBook)
   const [anchorEl, setAnchorEl] = useState(null)
+  const [isClick, setIsClick] = useState(false)
+
+  console.log('isClick', isClick)
 
   const optionOne = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour24: true }
   const optionTwo = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour24: true }
@@ -35,21 +39,37 @@ export default function Note({ blog, username = null }) {
   const timeInMillisecond = current - postedDay
 
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
+    if (isClick) {
+      setAnchorEl(event.currentTarget)
+    }
+
   }
 
   const handleClose = () => {
+    setIsClick(false)
     setAnchorEl(null)
   }
 
   const open = Boolean(anchorEl)
   const id = open ? 'simple-popover' : undefined
 
+  const handleView = () => {
+    if (!isClick) {
+      navigate(`/blogs/post/${blog.id}`)
+    }
+  }
+  const handleEdit = () => {
+    navigate(`/blogs/post/edit/${blog.id}`)
+
+  }
 
   return (
 
-    <Card sx={{ width: 345, height: 380 }}>
-      <CardActionArea>
+    <Card sx={{ width: 345, height: 380 }} >
+      <CardActionArea
+
+        onClick={handleView}
+      >
         <CardMedia
           sx={{ height: 200, width: '100%' }}
           component="img"
@@ -96,10 +116,17 @@ export default function Note({ blog, username = null }) {
                   new Date(Date.parse(blog.createdAt)).toLocaleString(undefined, optionThree)
               }
             </Box>
-            <IconButton aria-describedby={id} variant="contained" onClick={handleClick} >
+            <IconButton
+              onMouseOver={() => setIsClick(true)}
+              onMouseOut={() => setIsClick(false)}
+              aria-describedby={id}
+              variant="contained"
+              onClick={handleClick} >
               <MoreVertOutlinedIcon />
             </IconButton>
             <Popover
+              onMouseOver={() => setIsClick(true)}
+              onMouseOut={() => setIsClick(false)}
               id={id}
               open={open}
               anchorEl={anchorEl}
@@ -116,8 +143,8 @@ export default function Note({ blog, username = null }) {
 
 
               <Button><DeleteForeverOutlinedIcon /></Button>
-              <Button><EditNoteOutlinedIcon /></Button>
-              <Button><RemoveRedEyeOutlinedIcon /></Button>
+              <Button onClick={handleEdit}><EditNoteOutlinedIcon /></Button>
+              <Button onClick={handleView}><RemoveRedEyeOutlinedIcon /></Button>
             </Popover>
           </Box>
         </CardContent>
