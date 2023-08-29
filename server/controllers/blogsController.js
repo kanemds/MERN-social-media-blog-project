f5ccf650652051d9d05132d1a7c4b648897e74fe
Kanem <kanemdeng@email.com>
2023-08-28 18:44:12 -0700

    fixing the edit and upload images

--- server/controllers/blogsController.js
+++ server/controllers/blogsController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const Blog = require('../models/Blog')
-const isEqual = require('lodash/isEqual')
+const { isEqual, sortBy } = require('lodash')
 const storage = require('../config/firebaseConfig')
 
 const { ref, uploadBytes, getDownloadURL, deleteObject } = require('firebase/storage')
@@ -28,6 +28,81 @@ const processSingleImage = async images => {
   return singleImage
 }
 
+const filesUpload = async (files) => {
+  const imagesUrl = []
+  if (files) {
+    const uploadPromises = Object.keys(files).map(async (key) => {
+      try {
+        const name = ref(storage, `blogs/${new Date().getTime() + files[key].name}`)
+        const uploadImage = await uploadBytes(name, files[key].data)
+        const downloadImage = await getDownloadURL(uploadImage.ref)
+        await imagesUrl.push({ [key]: { url: downloadImage, name: files[key].name } })
+      } catch (error) {
+        console.error(`Error uploading file for key ${key}: ${error}`)
+      }
+    })
+    await Promise.all(uploadPromises) // Wait for all uploads to complete
+    return imagesUrl
+  } else {
+    console.log('giving default image')
+    return []
+  }
+}
+
+
+// const deleteImagesFromFireBase = async (files) => {
+//   const deletePromises = files.map(async (image) => { // Use map instead of forEach
+
+//     const desertRef = ref(storage, image.url)
+//     await deleteObject(desertRef)
+//   })
+
+//   await Promise.all(deletePromises) // Wait for all delete operations to complete
+//   console.log('Images have been removed from FireBase')
+// }
+
+
+
+const deleteImagesFromFirebase = async (imageUrls) => {
+
+  // Create an array of promises for deleting images
+  const deletePromises = imageUrls.map(async (imageUrl) => {
+    const imageRef = ref(storage, imageUrl.url)
+    await deleteObject(imageRef)
+    console.log(`Deleted image: ${imageUrl.url}`)
+  })
+
+  try {
+    // Wait for all delete operations to complete
+    await Promise.all(deletePromises)
+    console.log('All images have been deleted from Firebase Storage')
+  } catch (error) {
+    console.error('Error deleting images:', error)
+  }
+}
+
+const sortAndCombine = (imagesFromOrigin, imagesFromFireBase) => {
+  return imagesFromOrigin.concat(imagesFromFireBase).sort((a, b) => {
+    return parseInt(Object.keys(a)[0]) - parseInt(Object.keys(b)[0])
+  })
+}
+const singleSort = (images) => {
+  images.sort((a, b) => {
+    return parseInt(Object.keys(a)[0]) - parseInt(Object.keys(b)[0])
+  })
+}
+
+const removeOrderNumber = (images) => {
+  return images?.map(image => {
+    const key = Object.keys(image)[0]
+    return image[key]
+  })
+}
+
+const findEqual = (arrayOne, arrayTwo) => {
+  return isEqual(sortBy(arrayOne, '_id'), sortBy(arrayTwo, '_id'))
+}
+
 
 // @desc Get all blogs
 // route Get /blogs
@@ -52,7 +127,6 @@ const getAllBlogs = async (req, res) => {
 // @access Private
 const getSingleBlog = async (req, res) => {
   const { id } = req.params
-
   const blog = await Blog.findById(id).exec()
 
 
@@ -127,72 +201,24 @@ const createBlog = async (req, res) => {
 // @access Private
 const updateBlog = async (req, res) => {
 
+  const images = []
+  let imagesFromDL = []
+  let newImages
+  let imagesToServer
 
-  const orgImages = { ...req.body }
+  const { id, title, text, visibleTo, ...orgImages } = req.body // ...orgImages = ...req.body rename (type of orgImages === 'object')
   const fileObject = { ...req.files } // if [...req.files] not not iterable, prevent mapping
-  // console.log('inputObject', inputObject) // return as obj
+  // console.log('orgImages', orgImages) // return as obj even though single or multiples
   // console.log('fileObject', fileObject) // return as obj single or multiples
 
-
-
-
-  const { id, title, text, visibleTo } = req.body
-
-  const images = []
-
-  // Object.keys or Object.values return an array
-  Object.keys(orgImages).forEach((key) => {
-    if (!isNaN(key)) {
-      images.push({ [key]: JSON.parse(orgImages[key]) })
-    }
-  })
-
-
-  const filesUpload = async (files) => {
-    const imagesUrl = []
-    if (files) {
-      const uploadPromises = Object.keys(files).map(async (key) => {
-        // console.log('key', key)
-        // console.log('files[key]', files[key])
-        // console.log('files[key]', files[key].name)
-
-        const name = ref(storage, `blogs/${new Date().getTime() + files[key].name}`)
-        const uploadImage = await uploadBytes(name, files[key].data)
-        const downloadImage = await getDownloadURL(uploadImage.ref)
-        imagesUrl.push({ [key]: { url: downloadImage, name: files[key].name } })
-      })
-
-      await Promise.all(uploadPromises) // Wait for all uploads to complete
-      return imagesUrl
-    } else {
-      console.log('giving default image')
-      return []
-    }
-  }
-
-  const imagesFromDL = await filesUpload(fileObject)
-
-  const newImages = images.concat(imagesFromDL).sort((a, b) => {
-    return parseInt(Object.keys(a)[0]) - parseInt(Object.keys(b)[0])
-  })
-
-  const test = newImages.map(image => {
-    const key = Object.keys(image)[0]
-    return image[key]
-  })
-
-  // console.log('test', test)
-
-
+  // console.log(orgImages)
 
   if (!id || !title || !text || !visibleTo) {
     return res.status(400).json({ message: 'All fields are required' })
   }
 
   const blog = await Blog.findById(id).exec()
-
   // console.log(blog)
-
 
   if (!blog) {
     return res.status(400).json({ message: 'Blog not found' })
@@ -204,39 +230,86 @@ const updateBlog = async (req, res) => {
     return res.status(409).json({ message: 'Title has been used' })
   }
 
-  // const isEqual = (one, two) => {
-  //   return one.url === two.url
+  if (Object.values(orgImages).length) {
+    // convert from string to obj and using Object.keys or Object.values return an array
+    Object.keys(orgImages).forEach((key) => {
+      images.push({ [key]: JSON.parse(orgImages[key]) })
+    })
+  } else {
+    console.log('no orgImages')
+  }
+
+
+
+  if (Object.values(fileObject).length) {
+    // // upload the update imageFiles and download from firebase
+    imagesFromDL = await filesUpload(fileObject)
+  } else {
+    console.log('no new imageFile(s)')
+  }
+
+
+  // // images with order number
+
+
+
+  // handle if both or either one
+  // sort the order from originImages and images dl from firebase
+  newImages = await sortAndCombine(images, imagesFromDL)
+  // console.log('newImages', newImages)
+
+  // remove from [1:{},2:{}] to [{}:{}]
+  const newSetOrderImages = await removeOrderNumber(newImages)
+
+
+  console.log('blog.images', blog.images)
+
+  console.log('newSetOrderImages', newSetOrderImages)
+
+
+  // // check if order and file _id same
+  // const orgImagesId = blog?.images.map(image => ({
+  //   _id: image._id.toString()
+  // }))
+
+  // const newImagesId = await newSetOrderImages?.map(image => ({
+  //   _id: image._id
+  // }))
+
+  // const isMatch = findEqual(orgImagesId, newImagesId)
+
+  // console.log(isMatch)
+
+
+
+  // -------------------------------------------------
+
+  // if (images.length >= 1 && !imagesFromDL.length && isMatch) {
+  //   imagesToServer = newSetOrderImages
   // }
 
-  // const isMatch = blog.images.some(orgImage => test.some(newImage => isEqual(orgImage, newImage)))
-
-  // const orgImages = blog.images(image => ...image, _id: _id.toString())
-
-  const orgImagesId = blog.images.map(image => ({
-    _id: image._id.toString()
-  }))
-
-  const newImagesId = test.map(image => ({
-    _id: image._id
-  }))
-
-  console.log('orgImagesId', orgImagesId)
-  console.log('newImagesId', newImagesId)
-  const isMatch = isEqual(orgImagesId, newImagesId)
-
-  console.log(isMatch)
+  const imagesUrlNotMatch =
 
 
+
+
+
+  //   if (!isMatch) {
+  //     // remove all images from origin and update new set of images
+  //     await deleteImagesFromFirebase(blog.images)
+  //   } else {
+  //     console.log('same image(s)')
+  //   }
 
   // blog.title = title
   // blog.text = text
-  // blog.images = processedImages
+  // blog.images = imagesToServer
   // blog.visibleTo = visibleTo
 
-  //   const updatedBlog = await blog.save()
+  // const updatedBlog = await blog.save()
 
-  //   res.json(`${updatedBlog.title} updated`)
-  // }
+  // res.json(`${updatedBlog.title} updated`)
+
 }
 // @desc Delete a blog
 // route Delete /blogs
