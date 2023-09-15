4200d8116a37a071ec3191f2b36627f45d3b6e1b
Kanem <kanemdeng@email.com>
2023-09-15 16:26:53 -0700

    like page is avaiable, starts with decending order and adjust with button

--- client/src/pages/likes/LikeList.js
+++ client/src/pages/likes/LikeList.js
@@ -53,36 +53,17 @@ const buttonStyle = {
 }
 
 
-const dataList = [{ id: 1, 'type': 'All' }, { id: 2, 'type': 'Public' }, { id: 3, 'type': 'Private' }]
-
 const LikeList = () => {
 
 
   const { username } = useAuth()
 
-  const [isSelected, setIsSelected] = useState('All')
   const [isDesc, setIsDesc] = useState(true) // high to low
-  const [currentUserBlogs, setCurrentUserBlogs] = useState(null)
+  const [currentLikes, setCurrentLikes] = useState(null)
+  const [isReady, setIsReady] = useState(false)
   const [searchInput, setSearchInput] = useState('')
   const [searchResult, setSearchResult] = useState(null)
   const [isSearch, setIsSearch] = useState(false)
-
-  const {
-    data,
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  } = useGetBlogsQuery()
-
-
-  // const { likeList } = useGetLikedBlogsFromUserQuery(username, {
-  //   selectFromResult: ({ data }) => ({
-  //     likeList: Object.values(data.entities)
-  //   })
-  // })
-
-  // console.log(likeList)
 
   const { likedBlogs } = useGetUserLikedBlogsQuery(username, {
     selectFromResult: ({ data }) => ({
@@ -90,26 +71,18 @@ const LikeList = () => {
     })
   })
 
-  console.log(likedBlogs)
-
-
   useEffect(() => {
-    if (isSuccess) {
-      const findUserBlogs = Object.values(data?.entities)?.filter(blog => blog.user === username)
-      const descendingOrder = findUserBlogs?.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-      setCurrentUserBlogs(descendingOrder)
+
+    if (likedBlogs) {
+      setCurrentLikes(Object.values(likedBlogs))
+      setIsReady(true)
     }
+  }, [likedBlogs])
 
-  }, [isSuccess])
-
-
-  const handleSelect = (e) => {
-    setIsSelected(e.target.value)
-  }
 
   let content
 
-  if (isLoading) {
+  if (isReady === false) {
     content = (
       <Box sx={{ minHeight: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
         <LoadingSpinner />
@@ -120,16 +93,17 @@ const LikeList = () => {
 
   const handleAscendent = () => {
     if (isDesc) {
-      const ascendingOrder = currentUserBlogs?.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
-      setCurrentUserBlogs(ascendingOrder)
+
+      const ascendingOrder = currentLikes?.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+      setCurrentLikes(ascendingOrder)
       setIsDesc(false)
     }
   }
 
   const handleDescendent = () => {
     if (!isDesc) {
-      const descendingOrder = currentUserBlogs?.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-      setCurrentUserBlogs(descendingOrder)
+      const descendingOrder = currentLikes?.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+      setCurrentLikes(descendingOrder)
       setIsDesc(true)
     }
   }
@@ -138,10 +112,9 @@ const LikeList = () => {
     if (!searchInput.length) return console.log('nothing')
     const inputLowerCase = searchInput.toLowerCase()
     // console.log([...inputLowerCase]) // ['s', 'd', 'f', 'd', 's']
-    const result = currentUserBlogs.filter(blog =>
+    const result = setCurrentLikes.filter(blog =>
       [inputLowerCase].some(character => blog.title.toLowerCase().includes(character) || blog.text.toLowerCase().includes(character))
     )
-
     if (!result.length) {
       setSearchInput('')
       setIsSearch(true)
@@ -151,43 +124,19 @@ const LikeList = () => {
       setIsSearch(true)
       return setSearchResult(result)
     }
-
   }
 
-  const searchPublicBlogs = Array.isArray(searchResult) && searchResult?.filter(blog => blog.visible_to === 'public')
-  const searchPrivateBlogs = Array.isArray(searchResult) && searchResult?.filter(blog => blog.visible_to === 'private')
-
-  const publicBlogs = currentUserBlogs?.filter(blog => blog.visible_to === 'public')
-  const privateBlogs = currentUserBlogs?.filter(blog => blog.visible_to === 'private')
-
-  if (isSuccess) {
+  if (isReady === true) {
 
     content = (
 
       <Grid container spacing={1} columns={{ xs: 12, sm: 12, md: 12, lg: 12, ll: 12, xl: 15, xxl: 12 }}>
-        {
-          isSelected === 'All' ?
-            (
-              currentUserBlogs?.map(blog =>
-                <Grid key={blog.id} xs={12} sm={12} md={6} lg={4} ll={3} xl={3} xxl={2} >
-                  <Note blog={blog} />
-                </Grid>)
-            ) :
-            isSelected === 'Public' ?
-              (
-                publicBlogs?.map(blog =>
 
-                  <Grid key={blog.id} xs={12} sm={12} md={6} lg={4} ll={3} xl={3} xxl={2} >
-                    <Note blog={blog} />
-                  </Grid>)
-              ) :
-              (
-                privateBlogs?.map(blog =>
-                  <Grid key={blog.id} xs={12} sm={12} md={6} lg={4} ll={3} xl={3} xxl={2} >
-                    <Note blog={blog} />
-                  </Grid>)
-              )
-        }
+        {currentLikes?.map(blog =>
+          <Grid key={blog.id} xs={12} sm={12} md={6} lg={4} ll={3} xl={3} xxl={2} >
+            <Note blog={blog} />
+          </Grid>)}
+
       </Grid>
     )
   }
@@ -195,19 +144,12 @@ const LikeList = () => {
   return (
 
     <Box sx={{ width: '100%' }} >
-      <Box sx={{ position: 'sticky', top: '70px', backgroundColor: 'white', zIndex: 10, width: '100%', pt: '10px', pb: '10px', pl: 2, pr: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center', }}>
-        <Box sx={{ width: '100%' }}>
+      <Box sx={{ position: 'sticky', top: '150px', backgroundColor: 'white', zIndex: 10, width: '100%', pt: '10px', pb: '10px', pl: 2, pr: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center', }}>
+        {/* <Box sx={{ width: '100%' }}>
           <ClientSearchBar setSearchInput={setSearchInput} searchInput={searchInput} handleSearch={handleSearch} />
-        </Box>
+        </Box> */}
         <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', alignItems: 'center', mt: 1 }}>
-          <Box>
-            {dataList?.map(category => {
-              return (
-                <Button style={buttonStyle} key={category.id} size='small' variant={isSelected === category.type ? 'contained' : 'text'} value={category.type} onClick={handleSelect} sx={{ minWidth: 0, mr: 2 }}>{category.type}</Button>
-              )
-            }
-            )}
-          </Box>
+
           <Box>
             {!isDesc ?
               <Button size='small' sx={{ minWidth: 0, p: 0 }} onClick={handleDescendent}>
@@ -225,43 +167,8 @@ const LikeList = () => {
           </Box>
         </Box>
       </Box>
-      <Box sx={{ p: 2 }}>
-
-        {Array.isArray(searchResult) && searchResult.length && isSearch ?
-
-          <Grid container spacing={2} columns={{ xs: 12, sm: 12, md: 12, lg: 12, ll: 12, xl: 15, xxl: 12 }}>
-
-            {
-              isSelected === 'All' ?
-                (
-                  searchResult?.map(blog =>
-                    <Grid key={blog.id} xs={12} sm={12} md={6} lg={4} ll={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}>
-                      <Note blog={blog} />
-                    </Grid>)
-                ) :
-                isSelected === 'Public' ?
-                  (
-                    searchPublicBlogs?.map(blog =>
-
-                      <Grid key={blog.id} xs={12} sm={12} md={6} lg={4} ll={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <Note blog={blog} />
-                      </Grid>)
-                  ) :
-                  (
-                    searchPrivateBlogs?.map(blog =>
-                      <Grid key={blog.id} xs={12} sm={12} md={6} lg={4} ll={3} xl={3} xxl={2} sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <Note blog={blog} />
-                      </Grid>)
-                  )
-            }
-          </Grid>
-          :
-          typeof searchResult === 'string' && searchResult.length && isSearch ?
-            <Box> {searchResult}</Box>
-            :
-            content
-        }
-
+      <Box sx={{ p: 2, mt: '60px' }}>
+        {content}
       </Box>
     </Box >
 
