9413246c9da56d66cb6e0a53a3054b77f935689b
Kanem <kanemdeng@email.com>
2023-10-17 13:34:48 -0700

    able to crop image with rotation

--- client/src/components/imageEditor/ImageEditor.js
+++ client/src/components/imageEditor/ImageEditor.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { Box, Button, Slider } from '@mui/material'
+import { Box, Button, Slider, Typography } from '@mui/material'
 import Cropper from 'react-easy-crop'
-import getCroppedImg from './CropImage'
+// import getCroppedImg from './CropImage'
+import { getCroppedImg, getRotatedImage } from './CropImage'
 
 const ImageEditor = ({ avatarImage, setAvatarImage, setCroppedImg, handleClose, setOpen }) => {
 
@@ -10,6 +11,7 @@ const ImageEditor = ({ avatarImage, setAvatarImage, setCroppedImg, handleClose, 
 
   const [crop, setCrop] = useState({ x: 0, y: 0 })
   const [zoom, setZoom] = useState(1)
+  const [rotation, setRotation] = useState(0)
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
 
   const onCropChange = (crop) => {
@@ -25,41 +27,72 @@ const ImageEditor = ({ avatarImage, setAvatarImage, setCroppedImg, handleClose, 
     setZoom(zoom)
   }
 
+  const onRotationChange = (rotation) => {
+    setRotation(rotation)
+  }
+
   const saveImg = async () => {
-    const croppedImageUrl = await getCroppedImg(url, croppedAreaPixels)
+    const croppedImageUrl = await getCroppedImg(url, croppedAreaPixels, rotation)
     setCroppedImg(croppedImageUrl)
     setAvatarImage({ name: null, url: null })
     setOpen(false)
-
   }
 
+  const zoomPercent = value => {
+    return `${Math.round(value * 100)}%`
+  }
 
   return (
     <Box sx={{ height: '100%', width: '100%', position: 'relative' }}>
-      <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: '180px' }}>
+      <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: '180px', backgroundColor: 'black' }}>
         <Cropper
           image={url}
           crop={crop}
           zoom={zoom}
+          rotation={rotation}
           aspect={1}
           cropShape="round"
-          showGrid={false}
+          showGrid={true}
           onCropChange={onCropChange}
           onCropComplete={onCropComplete}
           onZoomChange={onZoomChange}
+          onRotationChange={setRotation}
+
         />
       </Box>
       <Box sx={{ display: 'flex', flexDirection: 'column', position: 'absolute', bottom: 0, height: '180px', alignItems: 'center', justifyContent: 'center', width: '100%' }} >
-        <Slider
-          value={zoom}
-          min={1}
-          max={3}
-          step={0.1}
-          aria-labelledby="Zoom"
-          onChange={(e, zoom) => onZoomChange(zoom)}
-          classes={{ container: 'slider' }}
-          sx={{ width: '50%', mb: 1 }}
-        />
+        <Box sx={{ width: '60%', }} textAlign='center'>
+          <Typography>{zoomPercent(zoom)}</Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Typography sx={{ width: '120px' }}>Zoom:</Typography>
+            <Slider
+              value={zoom}
+              // valueLabelFormat={zoomPercent}
+              min={1}
+              max={3}
+              step={1}
+              aria-labelledby="Zoom"
+              onChange={(e, zoom) => onZoomChange(zoom)}
+              classes={{ container: 'slider' }}
+            />
+          </Box>
+        </Box>
+        <Box sx={{ width: '60%', }} textAlign='center'>
+          <Typography >{rotation}°</Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Typography sx={{ width: '120px' }}>Rotation:</Typography>
+            <Slider
+              value={rotation}
+              min={0}
+              max={360}
+              step={1}
+              aria-labelledby="Rotation"
+              onChange={(e, rotation) => onRotationChange(rotation)}
+              classes={{ container: 'slider' }}
+
+            />
+          </Box>
+        </Box>
         <Box>
           <Button onClick={handleClose}>Cancel</Button>
           <Button onClick={saveImg}>Save</Button>
