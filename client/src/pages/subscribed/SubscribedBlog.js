147284108964a82e0e9e962efff80e35027d2920
Kanem <kanemdeng@email.com>
2023-10-20 16:20:25 -0700

    count total of subscribers for current blogger

--- client/src/pages/subscribed/SubscribedBlog.js
+++ client/src/pages/subscribed/SubscribedBlog.js
@@ -48,8 +48,6 @@ const styleDelete = {
 
 export default function SubscribedBlog({ blog, setRefresh, deleteSubscribed, removeMessage, isDeleteSubscribedLoading }) {
 
-  console.log(blog)
-
   const dispatch = useDispatch()
 
   const navigate = useNavigate()
